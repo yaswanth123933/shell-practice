c713715a1a67e7984ed8fe876458b63bdde0e97f
yaswanth <yaswanthch764@gmail.com>
2025-09-18 17:49:23 +0530

    some message

---
+++ 01-hello-world.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "join devops" #join devops 
---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo"Ramesh:: hi suresh,how are you"
+echo "suresh:: hello ramesh, iam fine .how are you
+
+echo "ramesh:: what are you doing"
