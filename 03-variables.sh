fc7283aaaa3369c19f73e417d4d4c30c55d43ce4
yaswanth <yaswanthch764@gmail.com>
2025-10-04 10:12:42 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#arguments
 PERSON1=$1
 PERSON2=$2
 
---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please enter your pin number"
+read PIN_NUMBER #here pin number is variable
+
+echo "PIN entered is: $PIN_NUMBER"
