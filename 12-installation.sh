fcb04a7aa51e12bf8ac050d75cd082fcaed6d818
yaswanth <yaswanthch764@gmail.com>
2025-10-04 15:35:46 +0530

    variables

---
+++ 12-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id-u)
+
+if [ $USERID -ne 0 ];then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 #failure is other than zero
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ];then
+    echo "ERROR:: Installing mysql is failure"
+    exit 1
+else
+    echo "Installing mysql is SUCCESS"
+    exit 0
+
+fi
