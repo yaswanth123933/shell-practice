1585b00fe9e66d61525e2cb5be890f4f9da2c73b
yaswanth <yaswanthch764@gmail.com>
2025-10-04 11:18:22 +0530

    variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -4,7 +4,7 @@ echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "script name: $0"
 echo "current directory: $PWD"
-echo "who is runningthis: $USER"
+echo "who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of the script: $$"
 sleep 50 &
---
+++ 09-data-types.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NUMBER1=100 #everything in shell is considered as string
+NUMBER2=200
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Sum is: ${SUM}"
