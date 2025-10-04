49be4ff3b9428acd7efb0ef1edc6dd1c708180ac
yaswanth <yaswanthch764@gmail.com>
2025-10-04 08:08:07 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+PERSON1=john
+PERSON2=yash 
+
 echo"Ramesh:: hi suresh,how are you"
-echo "suresh:: hello ramesh, iam fine .how are you
+echo "suresh:: hello ramesh, i am fine .how are you
 
 echo "ramesh:: what are you doing"
