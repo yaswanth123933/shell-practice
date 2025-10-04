af365e787eea063bfcd5803776165f9af9554d74
yaswanth <yaswanthch764@gmail.com>
2025-10-04 10:37:48 +0530

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestamp executed: $DATE"
+START_TIME=$(date +%s)
+
+slepp 10
+
+END_TIME=$(date +%S)
+TOTAL_TIME=$($END_TIME-$START_TIME)
+
+echo "Timestamp executed: $TOTAL_TIME Seconds"
