35c03543549b205688669b52f67e45d0e8aa6df0
yaswanth <yaswanthch764@gmail.com>
2025-10-04 08:17:29 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,7 +3,7 @@
 PERSON1=john
 PERSON2=yash 
 
-echo"Ramesh:: hi suresh,how are you"
+echo "Ramesh:: hi suresh,how are you"
 echo "suresh:: hello ramesh, i am fine .how are you"
 
 echo "ramesh:: what are you doing"
