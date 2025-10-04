4c790002765d940b556c3a9bf1938738765a12c8
yaswanth <yaswanthch764@gmail.com>
2025-10-04 10:04:41 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, finally not on zoom webinar"
+echo "$PERSON1:: so, do we have a deal"
+echo "$PERSON2:: Yes, if you make ukraine joins russia"
