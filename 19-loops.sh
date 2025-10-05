31a05ffdf7d1ca48a4bce9961a99a1aec5334294
yaswanth <yaswanthch764@gmail.com>
2025-10-05 06:30:26 +0530

    logs

---
+++ 19-loops.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+ R="\e[31m"
+ G="\e[32m"
+ Y="\e[33m"
+ N="\e[0m"
+ LOGS_FOLDER="/var/log/shell-script"
+ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 #failure is other than zero
+fi
+
+VALIDATE(){ #functions receive inputs through argus just like script argus
+   if [ $1 -ne 0 ];then
+      echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+
+      exit 1
+   else
+      echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+
+
+   fi
+}
+
+#$@ 
+
+for package in $@
+do 
+    echo "package is: $package
+done
